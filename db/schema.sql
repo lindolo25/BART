56e5123f83d836d9591f5c438ca9d629a31df30b
lindolo25 <lindolo25@outlook.com>
2018-12-13 21:15:25 -0500

    completed the Users modules

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
   username VARCHAR(100) NULL,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NULL,
-  rating_avg DECIMAL(10,4),
+  rating_avg DECIMAL(10,4) NOT NULL DEFAULT 0,
   age INT(3) NOT NULL,
   gender BOOLEAN NOT NULL,
   bio VARCHAR(120) NULL,
--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 require('dotenv').config();
 var db = require('./models');
+
+db.sequelize.sync();
