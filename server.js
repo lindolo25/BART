41f4372bf4d6d97e9b07c9a58d2db3aa14c5de98
lindolo25 <lindolo25@outlook.com>
2018-12-15 12:32:53 -0500

    worked in the server.js file, added some example views and public scc and html files

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
-var db = require('./models');
 
-db.sequelize.sync();
+
+var Express = require("express");
+var bodyParser = require("body-parser");
+var exphbs  = require('express-handlebars');
+var db = require('./models');
+const port = process.env.PORT || 3000;
+
+
+var app = Express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/assets",Express.static("public"));
+
+db.sequelize.sync().then(function()
+{
+    app.listen(port, () => console.log(`App listening on port ${port}!`));
+});
