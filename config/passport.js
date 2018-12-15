b8a20a6a3b86b75c585c3a0cdedbb0cabe9ba8e1
Barbie <bhernandez198@yahoo.com>
2018-12-15 13:36:51 -0500

    Login code needs to be changed to Sql

---
+++ config/passport.js
@@ -0,0 +1,16 @@
+var passport = require ('passport');
+var app = passport ();
+
+
+function passport () { 
+  passport.use(new LocalStrategy({
+    usernameField: 'user[email]',
+      passwordField: 'user[password]',
+      }, (email, password, done) => {
+      Users.findOne({ email })
+      .then((user) => {
+        if(!user || !user.validatePassword(password)) {
+        return done(null, false, { errors: { 'email or password': 'is invalid' } });
+            }
+          return done(null, user);
+      }).catch(done)
