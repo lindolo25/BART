cb2d64766ace4b5ef2016c47abf27423b7651ca6
AlessandraSchiller <a.schiller@umiami.edu>
2018-12-18 08:43:26 -0500

    still needs work

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
+
 $(document).ready(function() {
 $('.modal').modal();
 $('#modal1').open('onclick');
 $('#modal2').open('onclick');
 $('.modal-trigger').modal();
+$('select').material_select();
  });
+
+function changeOption() {
+	$('.all-options').hide();
+	$('#' + $('#options').val()).show();
+ }
