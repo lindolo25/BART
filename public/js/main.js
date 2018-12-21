8a643d8628c9d65d7e034caad35d9fa37391f0db
AlessandraSchiller <a.schiller@umiami.edu>
2018-12-20 19:30:55 -0500

    latest

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,14 @@
-
 $(document).ready(function() {
 $('.modal').modal();
-$('#modal1').open('onclick');
-$('#modal2').open('onclick');
+$('#modal1').on('click');
+$('#modal2').on('click');
 $('.modal-trigger').modal();
 $('select').material_select();
- });
+
 
 function changeOption() {
 	$('.all-options').hide();
 	$('#' + $('#options').val()).show();
  }
+
+  });
