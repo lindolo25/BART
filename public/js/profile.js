fd11a41956eb5c6937f279bd09fb000a27353639
Rolando Perez <lindolo25@outlook.com>
2018-12-24 11:38:25 -0500

    minor fixes to the UI

--- public/js/profile.js
+++ public/js/profile.js
@@ -18,7 +18,7 @@ function printAPIResponse(response)
 {
     for(var i = 0; i < response.length; i++)
     {
-        var item =  $('<div class="col s4">\
+        var item =  $('<div class="col s12 m4">\
                 <div class="card blue-grey darken-1">\
                     <div class="card-content white-text">\
                         <p>'+ response[i].comment +'</p>\
